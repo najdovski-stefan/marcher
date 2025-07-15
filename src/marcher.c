775864a7083bdeaa842dfd7dc99adef671cc6a49
stefan.najdovski <stefan.najdovski@aol.com>
2025-07-16 00:42:40 +0200

    now comes the hard part

--- src/marcher.c
+++ src/marcher.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "marcher.h"
 
 int main(){
 	write(1,"hello world\n",12);
---
+++ src/marcher.h
@@ -0,0 +1,12 @@
+#ifndef _MARCHER_H
+#define _MARCHER_H
+
+
+//something to start with
+typedef struct Marcher{
+  char* src;
+  int line;
+  int len;
+}Marcher;
+
+#endif //_MARCHER_H
